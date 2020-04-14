6ef940cb6a4d13ee9e60c536d896a0768f6c7d00
Tim DeHerrera <timothy.deherrera@smoothstack.com>
2020-04-13 22:36:09 -0600

    finish randomGame

---
+++ randomGame/com/ss/training/randomGame/PrintGame.java
@@ -0,0 +1,37 @@
+package com.ss.training.randomGame;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class PrintGame {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+
+		final int n = rand.nextInt(101);
+		
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Guess a number between 1-100 within the range of 10:");
+		
+		int num = reader.nextInt();
+		
+		for ( int i = 1; i < 5; i++ ) {
+			if ( num <= (n - 10) || num >= (n + 10) ) {
+				System.out.println("You failed to guess within a range of ten, you have " + (5-i) + " guesses left:");
+				num = reader.nextInt();
+			} else {
+				System.out.println("You win, the number is: " + n);
+				break;
+			}
+
+		}
+		
+		if ( num <= (n - 10) || num >= (n + 10) ) {
+			System.out.println("Sorry, you lost. The number was: " + n);
+		}
+		
+		reader.close();
+
+	}
+
+}
